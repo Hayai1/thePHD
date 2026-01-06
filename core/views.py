4de37e27318dcee2a8c25c6b6f782310926d6d05
dylan hough <dylanpjhough@gmail.com>
2026-01-06 00:00:50 +0000

    some home page changes

--- core/views.py
+++ core/views.py
@@ -4,6 +4,13 @@ from .models import Tour
 
 def home(request):
     tours = Tour.objects.all()
+    tours = [tour for tour in tours]
+    for tour in tours:
+        tour.venue = tour.venue.upper()
+        tour.date = tour.date.strftime("%d %b %Y").upper()
+        print("TOUR DATE:", tour.date)
+        tour.city = tour.city.upper()
+        tour.country = tour.country.upper()
     return render(request, 'home.html', {'tours': tours})
 
 def gigs(request):
